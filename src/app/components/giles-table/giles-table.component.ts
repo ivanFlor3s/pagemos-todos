9bf82ea3f7c715e7b112f9159cf915823bc11c3a
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-02-11 22:10:45 -0300

    Creo tabla component

---
+++ src/app/components/giles-table/giles-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-giles-table',
+  templateUrl: './giles-table.component.html',
+  styleUrls: ['./giles-table.component.css']
+})
+export class GilesTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
