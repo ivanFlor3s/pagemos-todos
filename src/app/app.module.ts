13b5b1b5b007e16bb9dcf20b4e65ae62d3d804b9
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-02-11 22:11:16 -0300

    importo componentes + rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { GilesTableComponent } from './components/giles-table/giles-table.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GilesTableComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
