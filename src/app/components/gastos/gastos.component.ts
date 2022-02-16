5bc0d597e608c90890647b15afcbc563c14a0b64
Ivan Flores <ifloresmancilla@est.frba.utn.edu.ar>
2022-02-15 23:41:13 -0300

    Toast en gastos + mar dirty all

--- src/app/components/gastos/gastos.component.ts
+++ src/app/components/gastos/gastos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { GilesListService } from '../../services/giles-list.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-gastos',
@@ -20,7 +21,8 @@ export class GastosComponent implements OnInit {
     return this.gastoForma.get('cuanto').invalid && this.gastoForma.get('cuanto').dirty 
   }
 
-  constructor(private fb: FormBuilder, private gilesService: GilesListService) {
+  constructor(private fb: FormBuilder, private gilesService: GilesListService,
+    private toastrService: ToastrService) {
     this.iniciarFormulario();
     this.escucharGilesIntegrantes()
   }
@@ -30,19 +32,30 @@ export class GastosComponent implements OnInit {
     this.gastoForma = this.fb.group({
       persona: ['', [Validators.required]],
       cuanto: ['', [Validators.required]],
-      descripcion: [''],
+      descripcion: ['Penes de madera'],
     });
   }
 
   escucharGilesIntegrantes(){
     this.gilesService.currentList.subscribe( giles => {
+      // console.log(giles)
       this.giles = giles.map(x => x.nombre)
+    
     })
+
+    
   }
 
   submitForm(){
+    console.log(this.gastoForma)
+
+    Object.keys(this.gastoForma.controls).map( control => {
+      this.gastoForma.controls[control].markAsDirty()
+    })
+
     if(this.gastoForma.invalid) return;
     const {persona, cuanto, descripcion} = this.gastoForma.value
     this.gilesService.agregarleGastoA(persona,cuanto,descripcion)
+    this.toastrService.info('Abrazo grando ' + persona ,'Le va a pagar Magosha!')
   }
 }
